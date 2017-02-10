73d1847e3c4936c406bbbad5951e765b5e279279
Vivek Kumar Singh <vksingh.iiitb@gmail.com>
2017-02-10 01:01:55 -0500

    Update facebook.R

--- facebook.R
+++ facebook.R
@@ -8,5 +8,15 @@ fb_oauth <- fbOAuth(app_id="***",
 fb_page <- getPage(page="cnn", token=fb_oauth,n=4000,feed = T,reactions = T)
 save.image(file="cnn.RData")
 
-
+#create corpus
+corpus1=vector()
+for( i in 1:length(fb_page$message))
+{
+  #browser()
+  msg1=fb_page$message[i]
+  msg1=gsub("[.]","",msg1)
+  msg1=gsub("[,]","",msg1)
+  msg1=strsplit(msg1," ")
+  corpus1=c(corpus1,unique(msg1[[1]]))
+}
 
