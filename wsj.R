623284fa655eca64d2eb528294abd66aff5022a9
Bhargav Thanki <bhargavthanky@gmail.com>
2017-02-22 11:59:43 -1200

    Create wsj.R

---
+++ wsj.R
@@ -0,0 +1,12 @@
+install.packages('Rfacebook',dependencies = T)
+library('Rfacebook')
+#create a favebook app and add http://localhost:1410/ to your app
+fb_oauth <- fbOAuth(app_id="***",
+                    app_secret="*****")
+
+fb_page <- getPage(page="wsj", token=fb_oauth,n=4000,feed = T,reactions = T)
+filename <- "wsj"
+filename <- paste(filename,Sys.Date())
+filename <- paste(filename,'.RData')
+
+save.image(file=filename)
