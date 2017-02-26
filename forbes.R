f64d9f87003c2e84d37eae4f96351a3a0fc86c6d
bmthanki <bhargavthanky@gmail.com>
2017-02-26 12:17:34 -0500

    Added /home

--- forbes.R
+++ forbes.R
@@ -1,7 +1,7 @@
 library('Rfacebook')
 load('/home/cis1024/facebook/m_facebook.RData')
 fb_page <- getPage(page="forbes", token=fb_oauth,n=4000,feed = T,reactions = T)
-filename <- "home/bhargav/datacollection/news-posts-with-R/data/forbes"
+filename <- "/home/bhargav/datacollection/news-posts-with-R/data/forbes"
 filename <- paste(filename,Sys.Date(),sep='')
 filename <- paste(filename,'.RData',sep='')
 save.image(file=filename)
--- nytimes.R
+++ nytimes.R
@@ -1,7 +1,7 @@
 library('Rfacebook')
 load('/home/cis1024/facebook/m_facebook.RData')
 fb_page <- getPage(page="nytimes", token=fb_oauth,n=4000,feed = T,reactions = T)
-filename <- "home/bhargav/datacollection/news-posts-with-R/data/nytimes"
+filename <- "/home/bhargav/datacollection/news-posts-with-R/data/nytimes"
 filename <- paste(filename,Sys.Date(),sep='')
 filename <- paste(filename,'.RData',sep='')
 save.image(file=filename)
