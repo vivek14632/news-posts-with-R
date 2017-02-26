bb2f596b7f6f3e64f806b36d31f6b88fbee18b0d
bmthanki <bhargavthanky@gmail.com>
2017-02-26 12:10:58 -0500

    Changes to get path

--- usatoday.R
+++ usatoday.R
@@ -1,7 +1,7 @@
 library('Rfacebook')
 load('/home/cis1024/facebook/m_facebook.RData')
 fb_page <- getPage(page="usatoday", token=fb_oauth,n=4000,feed = T,reactions = T)
-filename <- "usatoday"
+filename <- "home/bhargav/datacollection/news-posts-with-R/data/usatoday"
 filename <- paste(filename,Sys.Date(),sep='')
 filename <- paste(filename,'.RData',sep='')
 save.image(file=filename)
