7684eca91b3735c28689775ec6f348b9040fa700
Vivek Kumar Singh <vksingh.iiitb@gmail.com>
2017-03-07 14:31:46 -0500

    Create linearModel.R

---
+++ analysis/linearModel.R
@@ -0,0 +1,24 @@
+cnbc_data=as.data.frame(cbind(likes=fb_page$likes_count,
+                             message=fb_page$message,
+                             create_time=fb_page$created_time,
+                             today='2017-02-01',
+                             type=fb_page$type,
+                             link=fb_page$link,
+                             comments=fb_page$comments_count,
+                             shares=fb_page$shares_count,
+                             love=fb_page$love_count,
+                             haha=fb_page$haha_count,
+                             wow=fb_page$wow_count,
+                             sad=fb_page$sad_count,
+                             angry=fb_page$angry_count
+))
+
+cnbc_data$shares <- as.numeric(cnbc_data$shares)
+cnbc_data$love <- as.numeric(cnbc_data$love)
+cnbc_data$haha <- as.numeric(cnbc_data$haha)
+cnbc_data$wow <- as.numeric(cnbc_data$wow)
+cnbc_data$sad <- as.numeric(cnbc_data$sad)
+cnbc_data$angry <- as.numeric(cnbc_data$angry)
+
+cnbc_lm = lm(shares~ love +haha + wow + sad + angry,data = cnbc_data)
+summary(cnbc_lm)
