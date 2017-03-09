894bdc0fe76fc1178d8d5bae99c443dc9873bdc3
Vivek Kumar Singh <vksingh.iiitb@gmail.com>
2017-03-09 16:34:23 -0500

    Update linearModel.R

--- analysis/linearModel.R
+++ analysis/linearModel.R
@@ -1,19 +1,16 @@
-require(foreign)
-require(MASS)
-
 cnbc_data=as.data.frame(cbind(likes=as.numeric(fb_page$likes_count),
-                             message=fb_page$message,
-                             create_time=fb_page$created_time,
-                             today='2017-02-28',
-                             type=as.factor(fb_page$type),
-                             link=as.factor(fb_page$link),
-                             comments=as.numeric(fb_page$comments_count),
-                             shares=as.numeric(fb_page$shares_count),
-                             love=as.numeric(fb_page$love_count),
-                             haha=as.numeric(fb_page$haha_count),
-                             wow=as.numeric(fb_page$wow_count),
-                             sad=as.numeric(fb_page$sad_count),
-                             angry=as.numeric(fb_page$angry_count)
+                              message=fb_page$message,
+                              create_time=fb_page$created_time,
+                              today='2017-02-28',
+                              type=as.factor(fb_page$type),
+                              link=as.factor(fb_page$link),
+                              comments=as.numeric(fb_page$comments_count),
+                              shares=as.numeric(fb_page$shares_count),
+                              love=as.numeric(fb_page$love_count),
+                              haha=as.numeric(fb_page$haha_count),
+                              wow=as.numeric(fb_page$wow_count),
+                              sad=as.numeric(fb_page$sad_count),
+                              angry=as.numeric(fb_page$angry_count)
 ))
 
 cnbc_data$shares <- as.numeric(cnbc_data$shares)
@@ -31,8 +28,10 @@ cnbc_data$today <- strptime(cnbc_data$today,format = "%Y-%m-%d")
 cnbc_data$duration_in_days <- difftime(cnbc_data$today,cnbc_data$create_time,units = "days")
 
 
-model1 = lm(shares~ love +haha + wow + sad + angry+comments+likes,data = cnbc_data)
+model1 = lm(shares~ love +haha + wow + sad + angry+comments+likes+
+            duration_in_days,data = cnbc_data)
 summary(model1)
+
 opar <- par(mfrow = c(2,2), oma = c(0, 0, 1.1, 0))
 plot(model1, las = 1)
 par(opar)
