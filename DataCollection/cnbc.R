19191815273d07ff7669263f25be168e3e5273b7
Vivek Kumar Singh <vksingh.iiitb@gmail.com>
2017-03-09 07:29:10 -0500

    Update cnbc.R

--- DataCollection/cnbc.R
+++ DataCollection/cnbc.R
@@ -1,7 +1,9 @@
+source('dataLocation.R')
 library('Rfacebook')
 load('/home/cis1024/facebook/m_facebook1.RData')
-fb_page <- getPage(page="cnbc", token=fb_oauth,n=4000,feed = T,reactions = T)
-filename <- "/home/bhargav/datacollection/news-posts-with-R/data/cnbc"
+fb_page <- getPage(page="cnbc", token=fb_oauth,n=100,feed = T,reactions = T)
+#filename <- "/home/bhargav/datacollection/news-posts-with-R/data/cnbc"
+filename <- paste(dataDir,'cnbc',sep='')
 filename <- paste(filename,Sys.Date(),sep='')
 filename <- paste(filename,'.RData',sep='')
 save.image(file=filename)
