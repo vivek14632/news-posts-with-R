d054bafa935c1d0ab766a00a7640972026a5168d
Vivek Kumar Singh <vksingh.iiitb@gmail.com>
2017-03-09 07:24:07 -0500

    Update FoxNews.R

--- DataCollection/FoxNews.R
+++ DataCollection/FoxNews.R
@@ -1,7 +1,8 @@
 library('Rfacebook')
 load('/home/bhargav/appid.RData')
-fb_page <- getPage(page="FoxNews", token=fb_oauth,n=4000,feed = T,reactions = T)
-filename <- "/home/bhargav/datacollection/news-posts-with-R/data/FoxNews"
+fb_page <- getPage(page="FoxNews", token=fb_oauth,n=100,feed = T,reactions = T)
+#filename <- "/home/bhargav/datacollection/news-posts-with-R/data/FoxNews"
+filename <- paste(dataDir,'FoxNews',sep='')
 filename <- paste(filename,Sys.Date(),sep='')
 filename <- paste(filename,'.RData',sep='')
 save.image(file=filename)
