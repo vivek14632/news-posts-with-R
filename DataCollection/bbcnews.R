96effcc36390e0125bc31a839d947fa751fdce48
Vivek Kumar Singh <vksingh.iiitb@gmail.com>
2017-03-09 07:25:32 -0500

    Update bbcnews.R

--- DataCollection/bbcnews.R
+++ DataCollection/bbcnews.R
@@ -1,7 +1,8 @@
 library('Rfacebook')
 load('/home/cis1024/facebook/m_facebook.RData')
-fb_page <- getPage(page="bbcnews", token=fb_oauth,n=4000,feed = T,reactions = T)
-filename <- "/home/bhargav/datacollection/news-posts-with-R/data/bbcnews"
+fb_page <- getPage(page="bbcnews", token=fb_oauth,n=100,feed = T,reactions = T)
+#filename <- "/home/bhargav/datacollection/news-posts-with-R/data/bbcnews"
+filename <- paste(dataDir,'FoxNews',sep='')
 filename <- paste(filename,Sys.Date(),sep='')
 filename <- paste(filename,'.RData',sep='')
 save.image(file=filename)
