c26ade3fe9e23a4ad257ff953501f8bb20b88f66
Vivek Kumar Singh <vksingh.iiitb@gmail.com>
2017-03-09 07:32:46 -0500

    Update forbes.R

--- DataCollection/forbes.R
+++ DataCollection/forbes.R
@@ -1,7 +1,9 @@
+source('dataLocation.R')
 library('Rfacebook')
 load('/home/cis1024/facebook/m_facebook2.RData')
-fb_page <- getPage(page="forbes", token=fb_oauth,n=4000,feed = T,reactions = T)
-filename <- "/home/bhargav/datacollection/news-posts-with-R/data/forbes"
+fb_page <- getPage(page="forbes", token=fb_oauth,n=100,feed = T,reactions = T)
+#filename <- "/home/bhargav/datacollection/news-posts-with-R/data/forbes"
+filename <- paste(dataDir,'forbes',sep='')
 filename <- paste(filename,Sys.Date(),sep='')
 filename <- paste(filename,'.RData',sep='')
 save.image(file=filename)
