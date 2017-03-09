353518fd89ed4c1ddcc45d398b9728db8b070e15
Vivek Kumar Singh <vksingh.iiitb@gmail.com>
2017-03-09 07:38:00 -0500

    Update washingtonpost.R

--- DataCollection/washingtonpost.R
+++ DataCollection/washingtonpost.R
@@ -1,7 +1,10 @@
+source('dataLocation.R')
+filename <- paste(dataDir,'washingtonpost',sep='')
+
 library('Rfacebook')
 load('/home/bhargav/appid.RData')
-fb_page <- getPage(page="washingtonpost", token=fb_oauth,n=4000,feed = T,reactions = T)
-filename <- "/home/bhargav/datacollection/news-posts-with-R/data/washingtonpost"
+fb_page <- getPage(page="washingtonpost", token=fb_oauth,n=100,feed = T,reactions = T)
+#filename <- "/home/bhargav/datacollection/news-posts-with-R/data/washingtonpost"
 filename <- paste(filename,Sys.Date(),sep='')
 filename <- paste(filename,'.RData',sep='')
 save.image(file=filename)
