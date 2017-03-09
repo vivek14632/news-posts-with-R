b661772752763b5ce71331dc8974df1963d7cce7
Vivek Kumar Singh <vksingh.iiitb@gmail.com>
2017-03-09 07:34:04 -0500

    Update latimes.R

--- DataCollection/latimes.R
+++ DataCollection/latimes.R
@@ -1,7 +1,9 @@
+source('dataLocation.R')
 library('Rfacebook')
 load('/home/cis1024/facebook/m_facebook.RData')
-fb_page <- getPage(page="latimes", token=fb_oauth,n=4000,feed = T,reactions = T)
-filename <- "/home/bhargav/datacollection/news-posts-with-R/data/latimes"
+fb_page <- getPage(page="latimes", token=fb_oauth,n=100,feed = T,reactions = T)
+#filename <- "/home/bhargav/datacollection/news-posts-with-R/data/latimes"
+filename <- paste(dataDir,'latimes',sep='')
 filename <- paste(filename,Sys.Date(),sep='')
 filename <- paste(filename,'.RData',sep='')
 save.image(file=filename)
