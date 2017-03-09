f9c9c5b963f1b25d31a6a6a8d8b440e3f72f9b7e
Vivek Kumar Singh <vksingh.iiitb@gmail.com>
2017-03-09 07:47:05 -0500

    Create batchScript.sh

---
+++ DataCollection/batchScript.sh
@@ -0,0 +1,10 @@
+Rscript FoxNews.R
+Rscript bbcnews.R
+Rscript cnbc.R
+Rscript dataLocation.R
+Rscript forbes.R
+Rscript latimes.R
+Rscript nytimes.R
+Rscript usatoday.R
+Rscript washingtonpost.R
+Rscript wsj.R
