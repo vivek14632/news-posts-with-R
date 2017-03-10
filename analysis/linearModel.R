c083c44103e535e4b6ab63501ca949aeee12f6a7
bmthanki <bhargavthanky@gmail.com>
2017-03-09 20:02:46 -0500

    Revert "Changes for Days"
    This reverts commit df914216f0034605a68015f81de81e3e3e1f1c77.

--- analysis/linearModel.R
+++ analysis/linearModel.R
@@ -23,21 +23,17 @@ cnbc_data$wow <- as.numeric(cnbc_data$wow)
 cnbc_data$sad <- as.numeric(cnbc_data$sad)
 cnbc_data$angry <- as.numeric(cnbc_data$angry)
 
-cnbc_data$create_time <- strptime(cnbc_data$create_time,format = "%Y-%m-%d")
-cnbc_data$today <- strptime(cnbc_data$today,format = "%Y-%m-%d")
-cnbc_data$duration_in_days <- difftime(cnbc_data$today,cnbc_data$create_time,units = "days")
-
-
 model1 = lm(shares~ love +haha + wow + sad + angry+comments+likes,data = cnbc_data)
 summary(model1)
 opar <- par(mfrow = c(2,2), oma = c(0, 0, 1.1, 0))
-plot(model1, las = 1)
+plot(model, las = 1)
 par(opar)
 
 model2=rlm(shares~ love +haha + wow + sad + angry+comments+likes,data = cnbc_data)
 summary(model2)
+summary(model1)
 opar <- par(mfrow = c(2,2), oma = c(0, 0, 1.1, 0))
-plot(model2, las = 1)
+plot(model, las = 1)
 par(opar)
 
 
