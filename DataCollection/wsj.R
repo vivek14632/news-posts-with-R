10f505fff9ae2237764d20f0e11c96271b35545c
Vivek Kumar Singh <vksingh.iiitb@gmail.com>
2017-03-15 16:55:32 -0400

    Update wsj.R

--- DataCollection/wsj.R
+++ DataCollection/wsj.R
@@ -2,7 +2,7 @@ source('./dataLocation.R')
 filename <- paste(dataDir,'wsj',sep='')
 
 library('Rfacebook')
-load('/home/cis1024/facebook/m_facebook.RData')
+load('/home/cis1024/facebook/m_facebook1.RData')
 fb_page <- getPage(page="wsj", token=fb_oauth,n=100,feed = T,reactions = T)
 #filename <- "/home/bhargav/datacollection/news-posts-with-R/data/wsj"
 filename <- paste(filename,Sys.Date(),sep='')
