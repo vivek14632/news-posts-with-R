4864e045037db0fc3245caded599dd5076fb1937
bmthanki <bhargavthanky@gmail.com>
2017-03-29 21:46:50 -0400

    MasterDataCollection working fine

--- DataCollection/bbcnews.R
+++
@@ -1,9 +0,0 @@
-source('./dataLocation.R')
-library('Rfacebook')
-load('/home/cis1024/facebook/m_facebook1.RData')
-fb_page <- getPage(page="bbcnews", token=fb_oauth,n=100,feed = T,reactions = T)
-#filename <- "/home/bhargav/datacollection/news-posts-with-R/data/bbcnews"
-filename <- paste(dataDir,'bbcnews',sep='')
-filename <- paste(filename,Sys.Date(),sep='')
-filename <- paste(filename,'.RData',sep='')
-save.image(file=filename)
--- DataCollection/nytimes.R
+++
@@ -1,9 +0,0 @@
-source('./dataLocation.R')
-library('Rfacebook')
-load('/home/cis1024/facebook/m_facebook1.RData')
-fb_page <- getPage(page="nytimes", token=fb_oauth,n=100,feed = T,reactions = T)
-#filename <- "/home/bhargav/datacollection/news-posts-with-R/data/nytimes"
-filename <- paste(dataDir,'nytimes',sep='')
-filename <- paste(filename,Sys.Date(),sep='')
-filename <- paste(filename,'.RData',sep='')
-save.image(file=filename)
