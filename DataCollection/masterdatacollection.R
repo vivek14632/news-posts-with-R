e9107a9ab03e22038fee98282766a7e9b968bbb9
bmthanki <bhargavthanky@gmail.com>
2017-04-05 20:21:05 -0400

    Changes for nrows

--- DataCollection/masterdatacollection.R
+++ DataCollection/masterdatacollection.R
@@ -24,7 +24,7 @@ for(val in 1:nrow(newsagency)){
                        n.likes = fb_page$likes_count[value], n.comments = fb_page$comments_count[value])
     for(i in 1:length(posts))
     {
-      temp=rep(0,nrows(posts[[i]]$comments))
+      temp=rep(0,nrow(posts[[i]]$comments))
       posts[[i]]$comments=cbind(posts[[i]]$comments,reply=temp)
       for(j in 1:length(posts[[i]]$post$comments_count))
       {
