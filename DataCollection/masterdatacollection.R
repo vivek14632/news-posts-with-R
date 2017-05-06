0d2d0dd35007ef8e8f2ac37a477be1a77962606f
CIS1024 <vksingh.iiitb@gmail.com>
2017-05-06 13:42:58 -0400

    change

--- DataCollection/masterdatacollection.R
+++ DataCollection/masterdatacollection.R
@@ -5,24 +5,31 @@ library('Rfacebook')
 #getCommentReplies(comment_id=post$comments$id[1], token=fb_oauth)
 #auth <- data.frame('/home/cis1024/facebook/m_facebook.RData','/home/bhargav/appid.RData','/home/cis1024/facebook/m_facebook2.RData','/home/cis1024/facebook/m_facebook1.RData',stringsAsFactors=FALSE)
 #file_names=as.list(dir('/home/bhargav/facebook/rdata'))
-file_names=list.files(path = "/home/bhargav/facebook/rdata", full.names = TRUE, recursive = TRUE)
-auth=file_names
+
+#read tokens file
+tokensFile<-read.csv('/home/cis1024/projects/socialMediaProject/fbTokens.txt',header=T)
+auth=tokensFile$tokens
+
+
+#file_names=list.files(path = "/home/bhargav/facebook/rdata", full.names = TRUE, recursive = TRUE)
+#auth=file_names
 #auth <-lapply(file_names,load,.GlobalEnv)
 newsagency<-read.csv(file="https://raw.githubusercontent.com/bmthanki/news-posts-with-R/master/DataCollection/newsHandle.csv", sep=",",header = TRUE)
 for(val in 1:nrow(newsagency)){
-  auth[(val%%4)+1]
-  load(auth[(val%%4)+1])
-  print(load(auth[(val%%4)+1]))
-  fb_page <- getPage(page=newsagency$Handler[val], token=fb_oauth,n=100,feed = T,reactions = T)
+  fb_oauth=auth[(val%%3)+1]
+  #load(auth[(val%%4)+1])
+  #print(load(auth[(val%%4)+1]))
+	print(fb_oauth)
+  fb_page <- getPage(page=newsagency$Handler[val], token=toString(fb_oauth),n=100,feed = T,reactions = T)
   posts=list()
   replies=list()
   count1=1
-  #browser()
+  browser()
   for(value in 1:length(fb_page$id))
   {
     print(fb_page$id[value])
     #browser()
-    posts[[value]] <- getPost(fb_page$id[value], fb_oauth, comments = TRUE, likes = TRUE,
+    posts[[value]] <- getPost(fb_page$id[value], toString(fb_oauth), comments = TRUE, likes = TRUE,
                        n.likes = fb_page$likes_count[value], n.comments = fb_page$comments_count[value])
     for(i in 1:length(posts))
     {
@@ -36,7 +43,7 @@ for(val in 1:nrow(newsagency)){
           if(!is.na(posts[[i]]$comments$id[j]))
           {
             tryCatch({
-              replies[[count1]]=getCommentReplies(posts[[i]]$comments$id[j], token=fb_oauth)
+              replies[[count1]]=getCommentReplies(posts[[i]]$comments$id[j], token=toString(fb_oauth))
             posts[[i]]$comments$reply[j]=count1
             }, warning=function(w){
               #print(war)
