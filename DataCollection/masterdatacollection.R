ab4ba81730725e1fbb85db02cd2380190cc544b4
CIS1024 <vksingh.iiitb@gmail.com>
2017-05-06 19:45:44 -0400

    formatting

--- DataCollection/masterdatacollection.R
+++ DataCollection/masterdatacollection.R
@@ -24,54 +24,66 @@ for(val in 1:nrow(newsagency)){
 	{
 		next
 	}
-  fb_oauth=auth[(val%%3)+1]
-  #load(auth[(val%%4)+1])
-  #print(load(auth[(val%%4)+1]))
+  	fb_oauth=auth[(val%%3)+1]
 	print(fb_oauth)
-  fb_page <- getPage(page=newsagency$Handler[val], token=toString(fb_oauth),n=100,feed = T,reactions = T)
-  posts=list()
-  replies=list()
-  count1=1
-  browser()
-  for(value in 1:length(fb_page$id))
-  {
-    print(fb_page$id[value])
-    #browser()
-    posts[[value]] <- getPost(fb_page$id[value], toString(fb_oauth), comments = TRUE, likes = TRUE,
+  	fb_page <- getPage(page=newsagency$Handler[val], token=toString(fb_oauth),n=100,feed = T,reactions = T)
+  	posts=list()
+  	replies=list()
+  	count1=1
+  	#browser()
+
+	print(paste('Number of posts fetched=',length(fb_page$id)))
+
+	if(length(fb_page$id)<1)
+	{
+		#there are no posts so we will stop here
+		next
+	}
+  	for(value in 1:length(fb_page$id))
+  	{
+    		print(paste('Fetching data for post number =',fb_page$id[value]))
+    		#browser()
+    		posts[[value]] <- getPost(fb_page$id[value], toString(fb_oauth), comments = TRUE, likes = TRUE,
                        n.likes = fb_page$likes_count[value], n.comments = fb_page$comments_count[value])
-    for(i in 1:length(posts))
-    {
-      temp=rep(0,nrow(posts[[i]]$comments))
-      posts[[i]]$comments=cbind(posts[[i]]$comments,reply=temp)
-      if(posts[[i]]$post$comments_count>0)
-      {
-        print(posts[[i]]$post$comments_count)
-      for(j in 1:length(posts[[i]]$post$comments_count))
-      {
-          if(!is.na(posts[[i]]$comments$id[j]))
-          {
-            tryCatch({
-              replies[[count1]]=getCommentReplies(posts[[i]]$comments$id[j], token=toString(fb_oauth))
-            posts[[i]]$comments$reply[j]=count1
-            }, warning=function(w){
-              #print(war)
-            },error = function(e) {
-              #print(err)
-            }, finally = {
-              #print()
-            })
-          }
-        count1=count1+1
-        
-      }
-      }
-      
-    }
+	}
     
-  }
+	browser()
+	for(i in 1:length(posts))
+    	{
+		#create a temp vector to hold comments
+		#for a post
+		browser()
+      		temp=rep(0,nrow(posts[[i]]$comments))
+      		posts[[i]]$comments=cbind(posts[[i]]$comments,reply=temp)
+      		if(posts[[i]]$post$comments_count>0)
+      		{
+        		print(posts[[i]]$post$comments_count)
+      			for(j in 1:length(posts[[i]]$post$comments_count))
+      			{
+          			if(!is.na(posts[[i]]$comments$id[j]))
+          			{
+            				tryCatch({
+              					replies[[count1]]=getCommentReplies(posts[[i]]$comments$id[j], token=toString(fb_oauth))
+            					posts[[i]]$comments$reply[j]=count1
+            				}, warning=function(w){
+              				#print(war)
+            				},error = function(e) {
+              				#print(err)
+            				}, finally = {
+              				#print()
+            				})
+          			}
+        			count1=count1+1
+        
+      			}
+      		}
+      
+    	}
+    
+}
   
   #filename <- paste(dataDir,newsagency$Handler[val],sep='')
   #filename <- paste(filename,Sys.Date(),sep='')
   #filename <- paste(filename,'.RData',sep='')
-  save.image(file=filename)
-}
+save.image(file=filename)
+
