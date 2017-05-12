8c727b1d5f469f68566b92ce0135b16c66047067
CIS1024 <vksingh.iiitb@gmail.com>
2017-05-11 23:05:08 -0400

    stub code

---
+++ prediction/convertPostToWords.R
@@ -0,0 +1,10 @@
+# This function converts a post or string to set of words and  return 
+
+convertPostToWords<-function(post)
+{
+
+	#TODO
+
+	#dummy result vector
+	result<-c('hello','how','are','you')
+}
---
+++ prediction/createCorpus.R
@@ -0,0 +1,9 @@
+# This function returns corpus
+
+mCorpus<-function(posts)
+{
+
+
+	corpus<-c('hello','how','are','you')
+	return corpus
+}
