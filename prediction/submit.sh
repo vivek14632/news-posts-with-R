1d1abedbc1fa7e7e9f8b9336376d3af2d29e3924
vivek14632 <vksingh.iiitb@gmail.com>
2017-05-14 15:54:57 -0400

    circe script

---
+++ prediction/submit.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+#
+#SBATCH --comment=r-test
+#SBATCH --ntasks=1
+#SBATCH --job-name=r-test
+#SBATCH --output=output.%j.r-test
+#SBATCH --time=05:00:00
+#SBATCH --mem=30G
+#### SLURM 4 processor R test to run for 1 hour.
+
+module purge
+module add apps/R/3.1.2
+
+mpirun Rmpi tidy.R
