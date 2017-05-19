c6bb69c2641f5149b4c3306dc016d248f42761fb
Vivek Kumar Singh <vksingh.iiitb@gmail.com>
2017-05-19 07:35:02 -0400

    Update getDataFromURL.py

--- prediction/getDataFromURL.py
+++ prediction/getDataFromURL.py
@@ -14,18 +14,26 @@ import time
 from selenium.webdriver.common.action_chains import ActionChains
 from selenium.webdriver.firefox.firefox_binary import FirefoxBinary
 binary = FirefoxBinary('C:\Program Files (x86)\Mozilla Firefox\firefox.exe')
-fp=open('cnn.csv')
+fp=open('url.txt')
 urls=fp.readlines()
 fp.close()
 
+#The following line is commented because of this exception: selenium.common.exceptions.WebDriverException: Message: Unable to find a matching set of capabilities
+#driver = webdriver.Firefox(firefox_binary=binary)
+
+driver = webdriver.Firefox()
+
+
 for mline in urls:
 	mline=mline.split()
-	driver.get(mlinep[0])
+	mline=mline[0].split(',')
+	driver.get(mline[0])
 	content=driver.find_elements_by_xpath("//div[@class='zn-body__paragraph']")
 	fp=open(mline[1]+'.txt','w')
 	for i in content:
 		fp.write(i.text)
 	fp.close()
+	time.sleep(60)
 	
 
 
