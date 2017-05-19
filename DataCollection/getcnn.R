82a2aa1de83adf9a6f411697c7ff838d89f4da2d
Vivek Kumar Singh <vksingh.iiitb@gmail.com>
2017-05-19 16:18:55 -0400

    Create getcnn.R

---
+++ DataCollection/getcnn.R
@@ -0,0 +1,12 @@
+source('./dataLocation.R')
+source('./checkFileInDirectory.R')
+library('Rfacebook')
+
+
+#getCommentReplies(comment_id=post$comments$id[1], token=fb_oauth)
+#auth <- data.frame('/home/cis1024/facebook/m_facebook.RData','/home/bhargav/appid.RData','/home/cis1024/facebook/m_facebook2.RData','/home/cis1024/facebook/m_facebook1.RData',stringsAsFactors=FALSE)
+#file_names=as.list(dir('/home/bhargav/facebook/rdata'))
+
+#read tokens file
+tokensFile<-read.csv('/home/cis1024/projects/socialMediaProject/fbTokens.txt',header=T)
+auth=tokensFile$tokens
