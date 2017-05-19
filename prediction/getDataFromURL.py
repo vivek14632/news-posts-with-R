04297ae76ea903b513fd980453d68fb55eb1e6ef
CIS1024 <vksingh.iiitb@gmail.com>
2017-05-18 20:38:22 -0400

    cnn working

--- prediction/getDataFromURL.py
+++ prediction/getDataFromURL.py
@@ -1,1 +1,25 @@
 #this code is to get data from URL
+
+# install selenium
+# pip install selenium
+
+# download geko driver for windows or linux and put in your path
+
+
+import sys
+import urllib
+from random import randint
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+import time
+from selenium.webdriver.common.action_chains import ActionChains
+from selenium.webdriver.firefox.firefox_binary import FirefoxBinary
+binary = FirefoxBinary('C:\Program Files (x86)\Mozilla Firefox\firefox.exe')
+
+
+content=driver.find_elements_by_xpath("//div[@class='zn-body__paragraph']")
+
+for i in content:
+	i.text
+
+
