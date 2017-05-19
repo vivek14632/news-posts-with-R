40fca82f8e8d338c1317e157c40ea0f091a9d991
Vivek Kumar Singh <vksingh.iiitb@gmail.com>
2017-05-18 20:45:24 -0400

    Update getDataFromURL.py

--- prediction/getDataFromURL.py
+++ prediction/getDataFromURL.py
@@ -5,7 +5,6 @@
 
 # download geko driver for windows or linux and put in your path
 
-
 import sys
 import urllib
 from random import randint
@@ -15,11 +14,19 @@ import time
 from selenium.webdriver.common.action_chains import ActionChains
 from selenium.webdriver.firefox.firefox_binary import FirefoxBinary
 binary = FirefoxBinary('C:\Program Files (x86)\Mozilla Firefox\firefox.exe')
+fp=open('cnn.csv')
+urls=fp.readlines()
+fp.close()
 
+for mline in urls:
+	mline=mline.split()
+	driver.get(mlinep[0])
+	content=driver.find_elements_by_xpath("//div[@class='zn-body__paragraph']")
+	fp=open(mline[1]+'.txt','w')
+	for i in content:
+		fp.write(i.text)
+	fp.close()
+	
 
-content=driver.find_elements_by_xpath("//div[@class='zn-body__paragraph']")
-
-for i in content:
-	i.text
 
 
