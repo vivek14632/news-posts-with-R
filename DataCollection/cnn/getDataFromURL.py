8a6cd5bfa0d2bbf254ba1fa223384349b2ea17d9
Vivek Kumar Singh <vksingh.iiitb@gmail.com>
2017-05-20 20:41:53 -0400

    Update getDataFromURL.py

--- DataCollection/cnn/getDataFromURL.py
+++ DataCollection/cnn/getDataFromURL.py
@@ -4,7 +4,7 @@
 # pip install selenium
 
 # download geko driver for windows or linux and put in your path
-
+from random import *
 import sys
 import urllib
 from random import randint
@@ -13,17 +13,9 @@ from selenium.webdriver.common.keys import Keys
 import time
 from selenium.webdriver.common.action_chains import ActionChains
 from selenium.webdriver.firefox.firefox_binary import FirefoxBinary
-
-# the following code was used before, but not not rquired, but, still we should keep it
-#binary = FirefoxBinary('C:\Program Files (x86)\Mozilla Firefox\firefox.exe')
-
-#open the CSV with the CNN urls
+binary = FirefoxBinary('C:\Program Files (x86)\Mozilla Firefox\firefox.exe')
 fp=open('urls.csv')
-
-# lets read all URLs from file and also skip the header [0]
 urls=fp.readlines()[1:]
-
-#close the file
 fp.close()
 
 #The following line is commented because of this exception: selenium.common.exceptions.WebDriverException: Message: Unable to find a matching set of capabilities
@@ -31,32 +23,21 @@ fp.close()
 
 driver = webdriver.Firefox()
 
-#iterate for all URLs
+
 for mline in urls:
-	
-	#remove the new line character from the line
-	mline=mline.split()
-	
-	#split the columns since it is a CSV
-	mline=mline[0].split(',')
-	
-	#remove the quites from the URLs
-	mline[0]=mline[0].replace('"', '')
-	
-	#fetch the URLs
-	driver.get(mline[0])
-	
-	#get the content
-	content=driver.find_elements_by_xpath("//div[@class='zn-body__paragraph']")
-	
-	#write content to file
-	fp=open(mline[1]+'.txt','w')
-	for i in content:
-		fp.write(i.text)
-	fp.close()
-	
-	#sleep for one minute
-	time.sleep(60)
+	try:
+		mline=mline.split()
+		mline=mline[0].split(',')
+		mline[0]=mline[0].replace('"', '')
+		driver.get(mline[0])
+		content=driver.find_elements_by_xpath("//div[@class='zn-body__paragraph']")
+		fp=open(mline[1]+'.txt','w')
+		for i in content:
+			fp.write(i.text)
+		fp.close()
+		time.sleep(random()*20)
+	except:
+		pass
 	
 
 
