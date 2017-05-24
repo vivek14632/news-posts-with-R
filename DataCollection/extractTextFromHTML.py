19550b8619c790ec221aab82c02991fd27a8704e
Vivek Kumar Singh <vksingh.iiitb@gmail.com>
2017-05-24 11:55:29 -0400

    Create extractTextFromHTML.py

---
+++ DataCollection/extractTextFromHTML.py
@@ -0,0 +1,10 @@
+
+def extractTextFromHTML(mFileName)
+  fp=open(mFileName)
+  html_doc=fp.read()
+  fp.close()
+  from bs4 import BeautifulSoup
+  soup = BeautifulSoup(html_doc, 'html.parser')
+  [s.extract() for s in soup(['style', 'script', '[document]', 'head', 'title'])]
+  visible_text = soup.getText()
+  return visible_text
